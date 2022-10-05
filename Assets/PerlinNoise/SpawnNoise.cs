84da8281b79eda36f71f10274f4d310bd889172e
Oscar-Skinner <1027514@student.sae.edu.au>
2022-10-05 22:17:07 +1100

    add the cubes created for the perlin noise to a list. couldnt figure out spawning the cubes in waves.

--- Assets/PerlinNoise/SpawnNoise.cs
+++ Assets/PerlinNoise/SpawnNoise.cs
@@ -1,33 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnNoise : MonoBehaviour
 {
     public GameObject Prefab;
+    public List<GameObject> cubeLand = new List<GameObject>();
 
     private int amount = 200;
 
     public float scale = 40;
     public float zoom;
-    
+
+    private float timer;
+    private float interval = 1f;
     private float x;
     private float y;
     private Vector3 prefabPosition;
+
     private void Start()
     {
         for (int positionX = 0; positionX < amount; positionX++)
-        {
+        { 
             for (int positionZ = 0; positionZ < amount; positionZ++)
             {
                 prefabPosition.x = positionX;
                 prefabPosition.y = Mathf.PerlinNoise((positionX * zoom), (positionZ * zoom)) * scale;
                 prefabPosition.z = positionZ;
-                Instantiate(Prefab, prefabPosition, Quaternion.identity);
+                GameObject newCube = Instantiate(Prefab, prefabPosition, Quaternion.identity);
+                cubeLand.Add(newCube.gameObject);
             }
         }
-        
     }
 
 }
