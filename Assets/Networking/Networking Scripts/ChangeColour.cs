c74fd1629c02bf2dc3c7fe84a65703d814ed90fe
Oscar-Skinner <1027514@student.sae.edu.au>
2023-05-31 17:02:00 +1000

    more networking stuff

--- Assets/Networking/Networking Scripts/ChangeColour.cs
+++ Assets/Networking/Networking Scripts/ChangeColour.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class ChangeColour : MonoBehaviour
+public class ChangeColour : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NetworkVariable<Vector4> ColorValue  = new NetworkVariable<Vector4>();
+
+    /*
+    public override void OnNetworkSpawn()
     {
-        
+        if (IsOwner)
+        {
+            ChangeColor();
+        }
     }
 
-    // Update is called once per frame
+    public void ChangeColor()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            Vector4 randomColour = GetRandomMaterialIndex();
+            SetColor(randomColour);
+        }
+        else
+        {
+            SubmitMaterialIndexRequestServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    void SubmitMaterialIndexRequestServerRpc(ServerRpcParams rpcParams = default)
+    {
+        int randomIndex = GetRandomMaterialIndex();
+        setMaterialIndex(randomIndex);
+    }
+
+    void SetColor(Vector4 color)
+    {
+        ColorValue .Value = index;
+        // Apply the material change to the object
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null && renderer.sharedMaterials.Length > index)
+        {
+            Material newMaterial = renderer.sharedMaterials[index];
+            renderer.material = newMaterial;
+        }
+    }
+    
+    static int GetRandomMaterialIndex()
+    {
+        return Random.Range(0, 3);
+    }
+
     void Update()
     {
-        
-    }
+        SetColor(ColorValue .Value);
+    }*/
 }
